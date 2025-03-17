885454e33645c7efc4240b012637c7696a6fdd06
Neo <neo.sud@gmail.com>
2025-03-16 17:10:48 -0700

    Disabled Screen Transitions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,20 @@
 import { Stack } from "expo-router";
 
 export default function Layout() {
- return (
-   <Stack>
-     <Stack.Screen name="index" options={{ headerShown: false }} />
-     <Stack.Screen name="auth/RegisterStep1" options={{ headerShown: false }} />
-     <Stack.Screen name="auth/RegisterStep2" options={{ headerShown: false }} />
-     <Stack.Screen name="auth/Login" options={{ headerShown: false }} />
-     <Stack.Screen name="tabs/Home" options={{ headerShown: false }} />
-     <Stack.Screen name="tabs/Explore" options={{ headerShown: false }} />
-     <Stack.Screen name="tabs/HeatMap" options={{ headerShown: false }} />
-     <Stack.Screen name="tabs/CreateHabits" options={{ headerShown: false }} />
-     <Stack.Screen name="tabs/Leaderboards" options={{ headerShown: false }} />
-     <Stack.Screen name="tabs/StreakAward" options={{ headerShown: false }} />
-     <Stack.Screen name="tabs/Profile" options={{ headerShown: false }} />
+  return (
+    <Stack screenOptions={{ animation: "none" }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="auth/RegisterStep1" options={{ headerShown: false }} />
+      <Stack.Screen name="auth/RegisterStep2" options={{ headerShown: false }} />
+      <Stack.Screen name="auth/Login" options={{ headerShown: false }} />
+      <Stack.Screen name="tabs/Home" options={{ headerShown: false }} />
+      <Stack.Screen name="tabs/Explore" options={{ headerShown: false }} />
+      <Stack.Screen name="tabs/HeatMap" options={{ headerShown: false }} />
+      <Stack.Screen name="tabs/CreateHabits" options={{ headerShown: false }} />
+      <Stack.Screen name="tabs/Leaderboards" options={{ headerShown: false }} />
+      <Stack.Screen name="tabs/StreakAward" options={{ headerShown: false }} />
+      <Stack.Screen name="tabs/Profile" options={{ headerShown: false }} />
 
-   </Stack>
- );
+    </Stack>
+  );
 }
